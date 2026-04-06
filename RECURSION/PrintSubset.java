7cafde8cb562af9c3fae0ee6e165f8d94b693e93
akashyadavam <akashyadavam585ak@gmail.com>
2026-04-06 13:59:28 +0530

    add new code

--- RECURSION/PrintSubset.java
+++ RECURSION/PrintSubset.java
@@ -1,16 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintSubset {
     public static void main(String[] args) {
         String s="abc";
-        Subset("",s,0);
+        List<String>a1=new ArrayList<>();
+        Subset("",s,0,a1);
+        System.out.println(a1);
     }
-    public static void  Subset(String ans, String s,int idx) {
+    public static void Subset(String ans, String s,int idx,List<String>a1) {
         if(idx==s.length()){
-            System.out.println(ans+" ");
+            // System.out.println(ans+" ");
+            a1.add(ans);
             return;
+            
         }
         char ch=s.charAt(idx);
-         Subset(ans+ch, s, idx+1);//pick
-        Subset(ans, s, idx+1);//skip
+         Subset(ans+ch, s, idx+1,a1);//pick
+        Subset(ans, s, idx+1,a1);//skip
       
     }
     
