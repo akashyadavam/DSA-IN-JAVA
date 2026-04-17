b88195b6e79639e7b1890e0c33433fab883a85ca
akashyadavam <akashyadavam585ak@gmail.com>
2026-04-17 21:12:21 +0530

    code added

--- STACKS/NextGreaterElement.java
+++ STACKS/NextGreaterElement.java
@@ -1,11 +1,27 @@
 package STACKS;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
-
+ 
 public class NextGreaterElement {
     public static void main(String[] args) {
-        
+        int arr[]={1,8,3,5,2,1,6,4};
+        int nge[]=new int [arr.length];
+         Stack<Integer> st=new  Stack<>();
+         for(int i=arr.length-1;i>=0;i--){
+           while(st.size()!=0&&arr[i]>=st.peek()) st.pop();
+             if(st.size()==0) nge[i]=-1;
+              else nge[i]=st.peek();
+                st.push(arr[i]);
+            }
+         
+    
+    for(int ele:nge){
+        System.out.println(ele+" ");
+
     }
-  
+    
 }
+}
+
