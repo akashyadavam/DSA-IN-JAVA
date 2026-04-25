2001f4f28717cffff9be4e4618e537d3d79a5f3f
akashyadavam <akashyadavam585ak@gmail.com>
2026-04-25 12:43:19 +0530

    added

--- BINARY TREES/LevelOrderTraversal.java
+++ BINARY TREES/LevelOrderTraversal.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 class Pair {
     Node node;
     
@@ -21,6 +24,15 @@ class Node {
 }
 
 
+// Pair object
+// │
+// ├── node → Node object
+// │        ├── val = 3
+// │        ├── left = address of another Node
+// │        └── right = address of another Node
+// │
+// └── level = 0 samjh jao ab asani s
+
 public class LevelOrderTraversal {
     public static void main(String[] args) {
          Node a = new Node(3);  
@@ -39,55 +51,119 @@ public class LevelOrderTraversal {
         c.left = f;
         c.right = g;
         e.right = h;
+         
+    //    levelorder( a);//ek hi line mein print
+        
 
-        levelorder(a);//yahal level nahi bhejnenge use bnaneyenge class mein
+
+        //levelorder( a);//level wise neter krke print 
+      
+
+      levelorder( a);//ye bhi level wise enter mrke print karega boss;
+
+
+
+
+
+    // m3    ArrayList<ArrayList<Integer>> ans= ( levelorder(a));
+    //   System.out.println(ans);
     }
 
-   // private static void levelorder(Node root) {
-    //     Queue<Pair> q = new LinkedList<>();int currentlevel=0;
-    //     q.add(new Pair(root,0));//yaha new pair object bnakar bhejenge jsimein root level ayenga kyoki hmne sirf root node bnayi hi lekin level bhi chaiye to pair new object bnakar bhej diya ab new object mein bn jayenga
+   private static void levelorder(Node root) {
+        Queue<Pair> q = new LinkedList<>();int currentlevel=0;
+        q.add(new Pair(root,0));
 
-    //     while (!q.isEmpty()) {
-    //          Pair front = q.remove();
-    //         //  Node node=front.node;
-    //         //  int level=front.level;
-    //         if(front.level!=currentlevel){
-    //             currentlevel++;
-    //             System.out.println();
-    //         }
-           
-    //         System.out.print(front.node.val + " ");
-
-    //         if (front.node.left != null) q.add(new Pair(front.node.left,front.level+1));
-    //           if (front.node.right != null) q.add(new Pair(front.node.right,front.level+1 ));
-
-    //     }
-    //     System.out.println();
-    // } 
-       private static void levelorder(Node root) {
-         Queue<Node> q = new LinkedList<>();
-         q.add(root);
-         while(q.size()!=0){
-            int size=q.size();
-
-            for(int i=0;i<size;i++){//particular level ke sare elemnt add honge ek bar mein hi
-                 Node temp = q.remove();
+        while (!q.isEmpty()) {
+             Pair front = q.remove();
             
-                System.out.print(temp.val+" ");
-                 if (temp.left != null) q.add(temp.left);//yaha se jitne elemnt add ho rhe hai left right ke 4 bache se size phir se 4 ho ja rha upar 4 temo banakr print to phir niche se add ho hi rhe hai to loop phir se chalenge jab ekdum null tab size =0 to loop chalenga hi nahi for loop
-            if (temp.right != null) q.add(temp.right);
-            }
-            System.out.println();
+            if(front.level!=currentlevel){
+                currentlevel++;
+                System.out.println();
+            }//enter ka kam yeh kr raha hain boss
+           
+            System.out.print(front.node.val + " ");
 
+            if (front.node.left != null) q.add(new Pair(front.node.left,front.level+1));
+              if (front.node.right != null) q.add(new Pair(front.node.right,front.level+1 ));
 
-
-         }
-
-       }
-
-
-
-
+        }
+        System.out.println();
     } 
+   
+   
+   
+
+
+
+
+   
+   //m2 //    private static void levelorder(Node root) {
+    //      Queue<Node> q = new LinkedList<>();
+    //      q.add(root);
+    //      while(q.size()!=0){
+    //         int size=q.size();
+
+    //         for(int i=0;i<size;i++){
+    //              Node temp = q.remove();
+            
+    //             System.out.print(temp.val+" ");
+    //              if (temp.left != null) q.add(temp.left);
+    //         if (temp.right != null) q.add(temp.right);
+    //         }
+    //         System.out.println();
+
+
+
+    //      }
+
+    //    }
+
+
+
+    //m1//
+        // private static void levelorder(Node root) {
+        //  Queue<Node> q = new LinkedList<>();
+        //  q.add(root);
+        //  while(q.size()!=0){
+        //       Node temp=q.remove();
+        //       System.out.print(temp.val+" ");
+        //          if (temp.left != null) q.add(temp.left);
+        //     if (temp.right != null) q.add(temp.right);
+        //     }
+        //     // System.out.println();
+
+
+
+        //  }
+
+       
+
+
+
+    //m3 //   private  static ArrayList<ArrayList<Integer>> levelorder(Node root) {
+    //      Queue<Node> q = new LinkedList<>();
+    //      q.add(root);
+    //       ArrayList<ArrayList<Integer>> a = new ArrayList<>();
+    //       while(q.size()!=0){
+    //         int size=q.size();
+    //          ArrayList<Integer> b = new ArrayList<>();
+    //         for(int i=1;i<=size;i++){
+    //             Node t=q.remove();
+    //             b.add(t.val);
+    //              if (t.left != null) q.add(t.left);
+    //           if (t.right != null) q.add(t.right);
+    //         }
+    //         a.add(b);
+           
+
+            
+
+    //       }
+    //       return a;
+         
+
+
+    //   }
+    }
     
 
