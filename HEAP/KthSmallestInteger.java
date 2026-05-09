40709af25c0390c71ad2117e5abe3b02f6cf6f81
akashyadavam <akashyadavam585ak@gmail.com>
2026-05-09 18:02:21 +0530

    code add

---
+++ HEAP/KthSmallestInteger.java
@@ -0,0 +1,19 @@
+package HEAP;
+import java.util.*;
+
+public class KthSmallestInteger {
+    public static void main(String[] args) {
+        
+       PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+       int arr[]={7,10,4,3,20,15};
+       int k=3;
+       for(int ele:arr){
+        pq.add(ele);
+        if(pq.size()>k) pq.remove();
+       }
+       System.out.println(pq.peek());
+          
+
+    }
+    
+}
