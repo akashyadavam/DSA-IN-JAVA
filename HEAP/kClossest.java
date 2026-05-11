9b11f06c047b3ec3ab3a40d1502762b00478b7ce
akashyadavam <akashyadavam585ak@gmail.com>
2026-05-12 00:13:10 +0530

    code added

---
+++ HEAP/kClossest.java
@@ -0,0 +1,72 @@
+package HEAP;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Triplet implements Comparable<Triplet> {
+
+    int dist;
+    int x;
+    int y;
+
+    Triplet(int dist, int x, int y) {
+        this.dist = dist;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int compareTo(Triplet t) {
+        return Integer.compare(this.dist, t.dist);
+    }
+}
+
+public class kClossest {
+
+    public static int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<Triplet> pq =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int[] a : points) {
+
+            int x = a[0];
+            int y = a[1];
+
+            int dist = x * x + y * y;
+
+            pq.add(new Triplet(dist, x, y));
+
+            if (pq.size() > k)
+                pq.remove();
+        }
+
+        int ans[][] = new int[k][2];
+
+        for (int i = 0; i < k; i++) {
+
+            Triplet top = pq.remove();
+
+            ans[i][0] = top.x;
+            ans[i][1] = top.y;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int points[][] = {
+                {1, 3},
+                {-2, 2},
+                {5, 8}
+        };
+
+        int k = 2;
+
+        int ans[][] = kClosest(points, k);
+
+        for (int i = 0; i < ans.length; i++) {
+
+            System.out.println(ans[i][0] + " " + ans[i][1]);
+        }
+    }
+}
